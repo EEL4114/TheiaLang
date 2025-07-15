7201fe936e9d38b1ceaf3fdd8628a187feb80ce0
Erik <erik@mechaweka.co.nz>
2025-07-15 15:18:49 +0200

    no longer need this

--- TheiaLang/AstPrinter.cs
+++ TheiaLang/AstPrinter.cs
@@ -30,7 +30,6 @@ static class AstPrinter
 
     private static void PrintFunction(FunctionDeclaration fn, TextWriter w, int indent)
     {
-        Console.WriteLine($"{fn.Name} {fn.Scope.FullName}");
         w.WriteLine($"{Indent(indent)}FunctionDeclaration: {fn.ReturnType} {fn.Name}()");
         PrintBlock(fn.Statements, w, indent + tab);
     }
