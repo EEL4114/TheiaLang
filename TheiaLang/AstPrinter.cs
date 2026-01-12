08538e4a66219e64b9f853e6bd30e2dea9d19b9d
Erik <erik@mechaweka.co.nz>
2026-01-12 17:31:47 +0100

    updated debug printers

--- TheiaLang/AstPrinter.cs
+++ TheiaLang/AstPrinter.cs
@@ -2,7 +2,6 @@ using TheiaLang;
 
 static class AstPrinter
 {
-    const int tab = 4;
     // TODO this seems redundant?
     public static void Print(ProgramNode program, TextWriter w, bool includeTimestamp = true)
         => PrintProgram(program, w, 0, includeTimestamp);
@@ -32,23 +31,23 @@ static class AstPrinter
     static void PrintStruct(StructDeclaration structDeclaration, TextWriter w, int indent)
     {
         w.WriteLine($"{Indent(indent)}StructDeclaration: {structDeclaration.Name} ({structDeclaration.ResolvedType.Size} B) {TryScope(structDeclaration.Scope!)}");
-        w.WriteLine($"{Indent(indent + tab)}Fields:");
+        w.WriteLine($"{Indent(indent + 1)}Fields:");
         foreach (TypeNamePair typeNamePair in structDeclaration.Fields)
-            PrintTypeNamePair(typeNamePair, w, indent + tab * 2);
+            PrintTypeNamePair(typeNamePair, w, indent + 2);
 
         if (structDeclaration.Functions.Count == 0)
             w.WriteLine();
 
-        w.WriteLine($"{Indent(indent + tab)}Functions:");
+        w.WriteLine($"{Indent(indent + 1)}Functions:");
         foreach (FunctionDeclaration function in structDeclaration.Functions)
-            PrintFunction(function, w, indent + tab * 2);
+            PrintFunction(function, w, indent + 2);
     }
 
     static void PrintUnion(UnionDeclaration unionDeclaration, TextWriter w, int indent)
     {
         w.WriteLine($"{Indent(indent)}UnionDeclaration: {unionDeclaration.Name} ({unionDeclaration.ResolvedType.Size} B){TryScope(unionDeclaration.Scope!)}");
         foreach (TypeNamePair variant in unionDeclaration.Variants)
-            PrintTypeNamePair(variant, w, indent + tab);
+            PrintTypeNamePair(variant, w, indent + 1);
         w.WriteLine();
     }
 
@@ -60,17 +59,17 @@ static class AstPrinter
     static void PrintFunction(FunctionDeclaration function, TextWriter w, int indent)
     {
         w.WriteLine($"{Indent(indent)}FunctionDeclaration: {function.TypeName} {function.Name}{TryScope(function.Scope!)}");
-        w.WriteLine($"{Indent(indent + tab)}Arguments:");
+        w.WriteLine($"{Indent(indent + 1)}Arguments:");
         foreach (TypeNamePair typeNamePair in function.Arguments)
-            PrintTypeNamePair(typeNamePair, w, indent + tab * 2);
-        PrintBlock(function.Statements, w, indent + tab);
+            PrintTypeNamePair(typeNamePair, w, indent + 2);
+        PrintBlock(function.Statements, w, indent + 1);
     }
 
     static void PrintBlock(List<IStatement> block, TextWriter w, int indent)
     {
         w.WriteLine($"{Indent(indent)}BlockSatement:");
         foreach (IStatement stmt in block)
-            PrintStatement(stmt, w, indent + tab);
+            PrintStatement(stmt, w, indent + 1);
         w.WriteLine();
     }
 
@@ -86,54 +85,54 @@ static class AstPrinter
                 w.WriteLine($"{Indent(indent)}VariableDeclaration: {TryType(vd.ResolvedType)}{vd.Name}" +
                             (vd.Init is not null ? " =" : ""));
                 if (vd.Init != null)
-                    PrintExpression(vd.Init, w, indent + tab);
+                    PrintExpression(vd.Init, w, indent + 1);
                 break;
             case AssignmentStatement a:
                 w.WriteLine($"{Indent(indent)}Assignment:");
-                PrintExpression(a.Target, w, indent + tab);
-                PrintExpression(a.Expression, w, indent + tab);
+                PrintExpression(a.Target, w, indent + 1);
+                PrintExpression(a.Expression, w, indent + 1);
                 break;
             case IfStatement ifStatement:
                 w.WriteLine($"{Indent(indent)}If:");
-                w.WriteLine($"{Indent(indent + tab)}Condition:");
-                PrintExpression(ifStatement.Condition, w, indent + tab * 2);
-                w.WriteLine($"{Indent(indent + tab)}Then:{TryScope(ifStatement.ThenScope)}");
-                PrintBlock(ifStatement.ThenBranch, w, indent + tab * 2);
+                w.WriteLine($"{Indent(indent + 1)}Condition:");
+                PrintExpression(ifStatement.Condition, w, indent + 2);
+                w.WriteLine($"{Indent(indent + 1)}Then:{TryScope(ifStatement.ThenScope)}");
+                PrintBlock(ifStatement.ThenBranch, w, indent + 2);
                 if (ifStatement.ElseBranch != null)
                 {
-                    w.WriteLine($"{Indent(indent + tab)}Else:{TryScope(ifStatement.ElseScope!)}");
-                    PrintBlock(ifStatement.ElseBranch, w, indent + tab * 2);
+                    w.WriteLine($"{Indent(indent + 1)}Else:{TryScope(ifStatement.ElseScope!)}");
+                    PrintBlock(ifStatement.ElseBranch, w, indent + 2);
                 }
                 break;
             case ForStatement forStatement:
                 w.WriteLine($"{Indent(indent)}For:{TryScope(forStatement.Scope)}");
-                w.WriteLine($"{Indent(indent + tab)}Initialiser:");
-                PrintStatement(forStatement.Initialiser!, w, indent + tab * 2);
-                w.WriteLine($"{Indent(indent + tab)}Condition:");
-                PrintExpression(forStatement.Condition!, w, indent + tab * 2);
-                w.WriteLine($"{Indent(indent + tab)}Iterator:");
-                PrintStatement(forStatement.Iterator!, w, indent + tab * 2);
-                PrintBlock(forStatement.Body, w, indent + tab * 2);
+                w.WriteLine($"{Indent(indent + 1)}Initialiser:");
+                PrintStatement(forStatement.Initialiser!, w, indent + 2);
+                w.WriteLine($"{Indent(indent + 1)}Condition:");
+                PrintExpression(forStatement.Condition!, w, indent + 2);
+                w.WriteLine($"{Indent(indent + 1)}Iterator:");
+                PrintStatement(forStatement.Iterator!, w, indent + 2);
+                PrintBlock(forStatement.Body, w, indent + 2);
                 break;
             case ReturnStatement r:
                     w.WriteLine($"{Indent(indent)}Return: {TryType(r.Expression?.ResolvedType)}");
                 if(r.Expression != null)
-                    PrintExpression(r.Expression, w, indent + tab);
+                    PrintExpression(r.Expression, w, indent + 1);
                     
                 break;
             case ExpressionStatement e:
                 w.WriteLine($"{Indent(indent)}Expression:");
-                PrintExpression(e.Expression, w, indent + tab);
+                PrintExpression(e.Expression, w, indent + 1);
                 w.WriteLine();
                 break;
             case CompoundAssignmentStatement compound:
                 w.WriteLine($"{Indent(indent)}CompoundAssignment: {compound.Op}");
-                PrintExpression(compound.Target, w, indent + tab);
-                PrintExpression(compound.Expression, w, indent + tab);
+                PrintExpression(compound.Target, w, indent + 1);
+                PrintExpression(compound.Expression, w, indent + 1);
                 break;
             default:
                 w.WriteLine($"{Indent(indent)}<unknown statement '{statement.GetType().Name}'>");
-                w.WriteLine($"{Indent(indent + tab)}<{statement}'>");
+                w.WriteLine($"{Indent(indent + 1)}<{statement}'>");
                 break;
         }
     }
@@ -156,53 +155,53 @@ static class AstPrinter
 
             case BinaryExpression bin:
                 w.WriteLine($"{Indent(indent)}BinaryExpression: {TryType(bin.ResolvedType)}{bin.Op}");
-                PrintExpression(bin.Left, w, indent + tab);
-                PrintExpression(bin.Right, w, indent + tab);
+                PrintExpression(bin.Left, w, indent + 1);
+                PrintExpression(bin.Right, w, indent + 1);
                 break;
             case CallExpression call:
                 w.WriteLine($"{Indent(indent)}Call: {TryType(call.ResolvedType)}");
-                w.WriteLine($"{Indent(indent + tab)}Target:");
-                PrintExpression(call.Target, w, indent + tab * 2);
-                w.WriteLine($"{Indent(indent + tab)}Arguments:");
+                w.WriteLine($"{Indent(indent + 1)}Target:");
+                PrintExpression(call.Target, w, indent + 2);
+                w.WriteLine($"{Indent(indent + 1)}Arguments:");
 
                 foreach (IExpression arument in call.Arguments)
-                    PrintExpression(arument, w, indent + tab * 2);
+                    PrintExpression(arument, w, indent + 2);
                 break;
             case UnaryExpression u:
                 w.WriteLine($"{Indent(indent)}UnaryExpression: {TryType(u.ResolvedType)}{u.Op}");
-                w.WriteLine($"{Indent(indent + tab)}Operand:");
-                PrintExpression(u.Operand, w, indent + tab * 2);
+                w.WriteLine($"{Indent(indent + 1)}Operand:");
+                PrintExpression(u.Operand, w, indent + 2);
                 break;
             case MemberAccessExpression mem:
                 w.WriteLine($"{Indent(indent)}MemberAccess: {TryType(mem.ResolvedType)}");
-                w.WriteLine($"{Indent(indent + tab)}Target: {TryType(mem.Target.ResolvedType)}");
-                PrintExpression(mem.Target, w, indent + tab * 2);
-                w.WriteLine($"{Indent(indent + tab)}Member: {TryType(mem.Member.ResolvedType)}{mem.Member.Name}");
+                w.WriteLine($"{Indent(indent + 1)}Target: {TryType(mem.Target.ResolvedType)}");
+                PrintExpression(mem.Target, w, indent + 2);
+                w.WriteLine($"{Indent(indent + 1)}Member: {TryType(mem.Member.ResolvedType)}{mem.Member.Name}");
                 break;
             case InstantiationExpression isnt:
                 string type = TryType(isnt.ResolvedType);
                 type = string.IsNullOrEmpty(type) ? isnt.TypeName : type;
                 w.WriteLine($"{Indent(indent)}Instantiation: {type}");
-                w.WriteLine($"{Indent(indent + tab)}Arguments:");
+                w.WriteLine($"{Indent(indent + 1)}Arguments:");
                 // TODO write the field names maybe?
                 foreach (IExpression arument in isnt.Arguments)
-                    PrintExpression(arument, w, indent + tab * 2);
+                    PrintExpression(arument, w, indent + 2);
                 break;
             case IndexExpression index:
                 w.WriteLine($"{Indent(indent)}IndexExpression: {TryType(index.ResolvedType)}");
-                w.WriteLine($"{Indent(indent + tab)}Target:");
-                PrintExpression(index.Target, w, indent + tab * 2);
-                w.WriteLine($"{Indent(indent + tab)}Index:");
-                PrintExpression(index.Index, w, indent + tab * 2);
+                w.WriteLine($"{Indent(indent + 1)}Target:");
+                PrintExpression(index.Target, w, indent + 2);
+                w.WriteLine($"{Indent(indent + 1)}Index:");
+                PrintExpression(index.Index, w, indent + 2);
                 break;
             case CastExpression cast:
                 w.WriteLine($"{Indent(indent)}Cast: {TryType(cast.ResolvedType)} {cast.CastKind}");
-                w.WriteLine($"{Indent(indent + tab)}Target:");
-                PrintExpression(cast.Target, w, indent + tab * 2);
+                w.WriteLine($"{Indent(indent + 1)}Target:");
+                PrintExpression(cast.Target, w, indent + 2);
                 break;
             default:
                 w.WriteLine($"{Indent(indent)}<unknown expression {expr.GetType().Name}>");
-                w.WriteLine($"{Indent(indent + tab)}<{expr}>");
+                w.WriteLine($"{Indent(indent + 1)}<{expr}>");
                 break;
         }
     }
@@ -211,9 +210,9 @@ static class AstPrinter
 
     #region Helpers
 
-    static string Indent(int n) => new string(' ', n);
-
+    static string Indent(int n) => new string('\t', n);
     static string TryType(TypeInfo? typeInfo) => string.IsNullOrEmpty(typeInfo?.TypeName) ? "" : $"{typeInfo.TypeName} ({typeInfo.Size} B) ";
     static string TryScope(Scope scope) => scope == null ? " | Scope: ---" : $" | Scope: '{scope.Name}' ";
+    
     #endregion
 }
--- TheiaLang/ScopePrinter.cs
+++ TheiaLang/ScopePrinter.cs
@@ -2,8 +2,6 @@ using TheiaLang;
 
 static class ScopePrinter
 {
-    const int tab = 4;
-
     public static void Print(string ProgramName, Scope scope, TextWriter w, bool includeTimestamp = true)
     {
         w.WriteLine($"Program: {ProgramName}");
@@ -19,18 +17,18 @@ static class ScopePrinter
         w.WriteLine($"{Indent(indent)}Scope: {scope.Name}");
         if (scope.Symbols != null)
         {
-            w.WriteLine($"{Indent(indent + tab)}Symbols:");
+            w.WriteLine($"{Indent(indent + 1)}Symbols:");
             foreach (KeyValuePair<string, SymbolInfo> symbol in scope.Symbols)
-                w.WriteLine($"{Indent(indent + tab * 2)}{symbol.Key}");
+                w.WriteLine($"{Indent(indent + 2)}{symbol.Key}");
         }
 
         if (scope.Children != null)
         {
-            w.WriteLine($"{Indent(indent + tab)}Children:");
+            w.WriteLine($"{Indent(indent + 1)}Children:");
             foreach (KeyValuePair<string, Scope> child in scope.Children)
-                PrintScope(child.Value, w, indent + tab * 2);
+                PrintScope(child.Value, w, indent + 2);
         }
     }
 
-    static string Indent(int n) => new string(' ', n);
+    static string Indent(int n) => new string('\t', n);
 }
