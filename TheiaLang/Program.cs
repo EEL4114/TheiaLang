cb4dee72c39a04fa677bf9def3b29150965307e5
EEL4114 <alphaspacex2004@gmail.com>
2026-09-19 16:26:02 +0200

    update logging

--- TheiaLang/Program.cs
+++ TheiaLang/Program.cs
@@ -1,27 +1,26 @@
 ﻿using System.Diagnostics;
+using System.Drawing;
 using TheiaLang;
+using static TimerGroup;
 
 const ConsoleColor PRELOAD_COL = ConsoleColor.DarkGray;
 const ConsoleColor LEXER_COL   = ConsoleColor.Cyan;
 const ConsoleColor PARSER_COL  = ConsoleColor.Yellow;
+const ConsoleColor ELAB_COL    = ConsoleColor.Blue;
 const ConsoleColor SEM_COL     = ConsoleColor.DarkRed;
 const ConsoleColor IRGEN_COL   = ConsoleColor.Green;
 const ConsoleColor LLVM_COL    = ConsoleColor.Magenta;
 
 const int BAR_CHARS = 100;
 
-Stopwatch compileTimer  = new Stopwatch();
-Stopwatch preloadTimer  = new Stopwatch();
-Stopwatch lexTimer      = new Stopwatch();
-Stopwatch parseTimer    = new Stopwatch();
-Stopwatch printTimer    = new Stopwatch();
-Stopwatch analysisTimer = new Stopwatch();
-Stopwatch IRGenTimer    = new Stopwatch();
-Stopwatch LLVMTimer     = new Stopwatch();
+Dictionary<TimerGroup, Stopwatch> timers = [];
+
+foreach(TimerGroup group in Enum.GetValues<TimerGroup>())
+    timers.Add(group, new Stopwatch());
 
 #region  Startup
 
-preloadTimer.Start();
+timers[TG_Preload].Start();
 
 const string OUTPUT_PATH  = "Output/";
 const string DEBUG_PATH  = "Debug/";
@@ -51,14 +50,17 @@ do
 
 Parser preloadParser = new Parser(preloadTokens);
 (ProgramNode preloadAST, Scope preloadScope) = preloadParser.ParseProgram("__preload__");
-using StreamWriter writer = new StreamWriter($"{DEBUG_PATH_REL}__preload__.ast");
-AstPrinter.Print(preloadAST, writer, false);
-using StreamWriter scopeWriter = new StreamWriter($"{DEBUG_PATH_REL}__preload__.scope");
-ScopePrinter.Print("__preload", preloadScope, scopeWriter);
 
-preloadTimer.Stop();
+#if PRINT_PRELOAD
+    using StreamWriter writer = new StreamWriter($"{DEBUG_PATH_REL}__preload__.ast");
+    AstPrinter.Print(preloadAST, writer, false);
+    using StreamWriter scopeWriter = new StreamWriter($"{DEBUG_PATH_REL}__preload__.scope");
+    ScopePrinter.Print("__preload", preloadScope, scopeWriter);
+#endif
 
-Log.Time("Preload took", preloadTimer.Elapsed.TotalMilliseconds, PRELOAD_COL);
+timers[TG_Preload].Stop();
+
+Log.Time("Preload took", timers[TG_Preload].Elapsed.TotalMilliseconds, PRELOAD_COL);
 
 #endregion
 
@@ -75,7 +77,14 @@ if (args[0].EndsWith(".tia"))
     if (!Path.Exists(args[0]) || !File.Exists(args[0]))
         Log.Error(15, $"File '{args[0]}' could not be found");
 
-    CompileFile(args[0][0..args[0].LastIndexOf('.')]);
+    long allocationsBefore = GC.GetTotalAllocatedBytes(false);
+
+    CompileFile(args[0][0..args[0].LastIndexOf('.')], writeAST: true, writeScope: true);
+
+    long allocated = GC.GetTotalAllocatedBytes(false) - allocationsBefore;
+
+    Log.Info($"Allocated:   {FormatBytes(allocated)}");
+    LogMemory();
 }
 else    // folder
 {
@@ -104,6 +113,8 @@ else    // folder
             Log.Info($"=== Testing {Path.GetFileName(tiaFile)} ===");
         }
 
+        // LogPeakMemory();
+
         Log.Info($"\n{allTia.Count()} files tested, {failures} failures.");
     }
     else
@@ -121,12 +132,17 @@ else    // folder
 int CompileFile(string filePath,
                 bool insertLogs = false,
                 bool timestamps = true,
-                bool writeLexerOutput = false)
+                bool writeLexerOutput = false,
+                bool writeAST = false,
+                bool writeScope = false)
 {
     string code = File.ReadAllText(filePath + ".tia");
 
-    compileTimer.Start();
-    lexTimer.Start();
+    foreach(TimerGroup group in Enum.GetValues<TimerGroup>())
+        timers[group].Stop();
+
+    timers[TG_Compile].Start();
+    timers[TG_Lexer].Start();
     Lexer lexer = new Lexer(code);
     List<Token> tokens = [];
     Token token;
@@ -147,59 +163,81 @@ int CompileFile(string filePath,
 
     Log.Link(lexPath, "Lexer tokens: ");
 
-    lexTimer.Stop();
-    parseTimer.Start();
+    timers[TG_Lexer].Stop();
+    timers[TG_Parser].Start();
 
     Parser parser = new Parser(tokens);
 
     (ProgramNode ast, Scope globalScope) = parser.ParseProgram(programName);
 
-    parseTimer.Stop();
+    timers[TG_Parser].Stop();
+    timers[TG_Elab].Start();
     (ast, globalScope) = new Elaboration().Lower(preloadScope, preloadAST, globalScope, ast);
-    printTimer.Start();
+    timers[TG_Elab].Stop();
 
-    string writerPath = $"{DEBUG_PATH_REL}{programName}.ast";
-    using StreamWriter writer = new StreamWriter(writerPath);
-    AstPrinter.Print(ast, writer, timestamps);
-    Log.Link(writerPath, "Preload AST: ");
-    string sw2Path = $"{DEBUG_PATH_REL}{programName}.scope";
-    using StreamWriter sw2 = new StreamWriter(sw2Path);
-    ScopePrinter.Print(filePath, globalScope, sw2);
-    Log.Link(sw2Path, "Scope Tree: ");
+    timers[TG_Print].Start();
+    if (writeAST)
+    {
 
+        string writerPath = $"{DEBUG_PATH_REL}{programName}.ast";
+        using StreamWriter writer = new StreamWriter(writerPath);
+        AstPrinter.Print(ast, writer, timestamps);
+        Log.Link(writerPath, "Preload AST: ");
+    }
+    if(writeScope)
+    {
+        string sw2Path = $"{DEBUG_PATH_REL}{programName}.scope";
+        using StreamWriter sw2 = new StreamWriter(sw2Path);
+        ScopePrinter.Print(filePath, globalScope, sw2);
+        Log.Link(sw2Path, "Scope Tree: ");
+    }
 
-    printTimer.Stop();
-    analysisTimer.Start();
+    timers[TG_Print].Stop();
+
+    timers[TG_Analyser].Start();
 
     (ast, globalScope) = new SemanticAnalyser().AnalyseProgram(ast, globalScope);
 
-    analysisTimer.Stop();
-    printTimer.Start();
-    string sw3Path = $"{DEBUG_PATH_REL}{programName}_full.scope";
-    using StreamWriter sw3 = new StreamWriter(sw3Path);
-    ScopePrinter.Print(filePath, globalScope, sw3);
-    Log.Link(sw3Path, "Full Scope Tree: ");
+    timers[TG_Analyser].Stop();
+    timers[TG_Print].Start();
 
-    string writer2Path = $"{DEBUG_PATH_REL}{programName}_full.ast";
-    using StreamWriter writer2 = new StreamWriter($"{DEBUG_PATH_REL}{programName}_full.ast");
-    AstPrinter.Print(ast, writer2, timestamps);
-    Log.Link(writer2Path, "Full AST: ");
-    printTimer.Stop();
+    if(writeScope)
+    {
+        string sw3Path = $"{DEBUG_PATH_REL}{programName}_full.scope";
+        using StreamWriter sw3 = new StreamWriter(sw3Path);
+        ScopePrinter.Print(filePath, globalScope, sw3);
+        Log.Link(sw3Path, "Full Scope Tree: ");
+    }
+    if(writeAST)
+    {
+        string writer2Path = $"{DEBUG_PATH_REL}{programName}_full.ast";
+        using StreamWriter writer2 = new StreamWriter($"{DEBUG_PATH_REL}{programName}_full.ast");
+        AstPrinter.Print(ast, writer2, timestamps);
+        Log.Link(writer2Path, "Full AST: ");
+    }
+
+    timers[TG_Print].Stop();
+    timers[TG_Layout].Start();
 
     LayoutCalc layout = new LayoutCalc(CompilationTarget.x86_64_windows);
     layout.GenerateLayout(ast);
 
+    timers[TG_Layout].Stop();
+    timers[TG_ConstFold].Start();
+
     ConstantFolding cf = new ConstantFolding(layout);
     cf.ConstFold(ast);
 
-    IRGenTimer.Start();
+    timers[TG_ConstFold].Stop();
+
+    timers[TG_IRGen].Start();
 
     string irgenPath = $"{OUTPUT_PATH_REL}{programName}.ll";
     new IRGenerator().Emit(ast, globalScope, irgenPath, layout, insertLogs);
     Log.Link(irgenPath, "IR: ");
 
-    IRGenTimer.Stop();
-    LLVMTimer.Start();
+    timers[TG_IRGen].Stop();
+    timers[TG_LLVM].Start();
 
     ProcessStartInfo psi = new ProcessStartInfo
     {
@@ -248,38 +286,41 @@ int CompileFile(string filePath,
             throw new Exception(stderr2);
     }
 
-    LLVMTimer.Stop();
-    compileTimer.Stop();
-
+    timers[TG_LLVM].Stop();
+    timers[TG_Compile].Stop();
     return 0;
 }
 
 #endregion
 
-double lexerTime  = lexTimer.Elapsed.TotalMilliseconds;
-double parserTime = parseTimer.Elapsed.TotalMilliseconds;
-double semTime    = analysisTimer.Elapsed.TotalMilliseconds;
-double IRgenTime  = IRGenTimer.Elapsed.TotalMilliseconds;
-double LLVMTime   = LLVMTimer.Elapsed.TotalMilliseconds;
+Dictionary<TimerGroup, double> times = [];
 
-Log.Time("Lexer took", lexerTime, LEXER_COL);
-Log.Time("Parser took", parserTime, PARSER_COL);
-Log.Time("Semantic Analysis took", semTime, SEM_COL);
-Console.WriteLine($"AST printing took {printTimer.ElapsedMilliseconds} ms");
-Log.Time("IR Generation took", IRgenTime, IRGEN_COL);
-Log.Time("LLVM took", LLVMTime, LLVM_COL);
+foreach(TimerGroup group in Enum.GetValues<TimerGroup>())
+    times.Add(group, timers[group].Elapsed.TotalMilliseconds);
 
-Console.WriteLine($"All Processes finished in {compileTimer.ElapsedMilliseconds} ms");
+Log.Time("Lexer took",              times[TG_Lexer],    LEXER_COL);
+Log.Time("Parser took",             times[TG_Parser],   PARSER_COL);
+Log.Time("Semantic Analysis took",  times[TG_Analyser], SEM_COL);
+Log.Time("AST printing took",       times[TG_Print],    ConsoleColor.White);
+Log.Time("IR Generation took",      times[TG_IRGen],    IRGEN_COL);
+Log.Time("LLVM took",               times[TG_LLVM],     LLVM_COL);
 
-double totalTime = lexerTime + parserTime + IRgenTime + LLVMTime;
+double miscTime = Math.Round(times[TG_Layout] + times[TG_ConstFold] + times[TG_Elab], 4);
+Log.Time("Misc", miscTime, ConsoleColor.Gray);
+
+Console.WriteLine($"All Processes finished in {times[TG_Compile]} ms");
+
+double totalTime = times.Values.Sum() - times[TG_Compile];
 
 #region Chart Printing
 
-int lexerChars  = (int)Math.Round(lexerTime / (double)totalTime * BAR_CHARS);
-int parserChars = (int)Math.Round(parserTime / (double)totalTime * BAR_CHARS);
-int semChars    = (int)Math.Round(semTime / (double)totalTime * BAR_CHARS);
-int IRgenChars  = (int)Math.Round(IRgenTime / (double)totalTime * BAR_CHARS);
-int llvmChars   = BAR_CHARS - lexerChars - parserChars - IRgenChars;
+int preloadChars = (int)Math.Round(times[TG_Preload]  / (double)totalTime * BAR_CHARS);
+int lexerChars   = (int)Math.Round(times[TG_Lexer]    / (double)totalTime * BAR_CHARS);
+int parserChars  = (int)Math.Round(times[TG_Parser]   / (double)totalTime * BAR_CHARS);
+int semChars     = (int)Math.Round(times[TG_Analyser] / (double)totalTime * BAR_CHARS);
+int IRgenChars   = (int)Math.Round(times[TG_IRGen]    / (double)totalTime * BAR_CHARS);
+int miscChars    = (int)Math.Round(miscTime           / (double)totalTime * BAR_CHARS);
+int llvmChars    = BAR_CHARS - lexerChars - parserChars - IRgenChars;
 
 static void PrintSegment(int count, ConsoleColor color)
 {
@@ -289,16 +330,46 @@ static void PrintSegment(int count, ConsoleColor color)
 }
 
 Console.Write("[");
-PrintSegment(lexerChars,  LEXER_COL);
-PrintSegment(parserChars, PARSER_COL);
-PrintSegment(semChars,    SEM_COL);
-PrintSegment(IRgenChars,  IRGEN_COL);
-PrintSegment(llvmChars,   LLVM_COL);
+PrintSegment(preloadChars,  PRELOAD_COL);
+PrintSegment(lexerChars,    LEXER_COL);
+PrintSegment(parserChars,   PARSER_COL);
+PrintSegment(semChars,      SEM_COL);
+PrintSegment(IRgenChars,    IRGEN_COL);
+PrintSegment(miscChars,     ConsoleColor.Gray);
+PrintSegment(llvmChars,     LLVM_COL);
 Console.WriteLine("]");
 
 #endregion
 
 #region Log
+
+
+
+static string FormatBytes(long bytes)
+{
+    const double MiB = 1024.0 * 1024.0;
+    return $"{bytes / MiB:F2} MiB";
+}
+
+
+static void LogMemory()
+{
+    using Process p = Process.GetCurrentProcess();
+    p.Refresh();
+
+    Console.WriteLine($"Working set: {FormatBytes(p.WorkingSet64)}");
+    Console.WriteLine($"Private:     {FormatBytes(p.PrivateMemorySize64)}");
+    // Console.WriteLine($"Virtual:     {FormatBytes(p.VirtualMemorySize64)}");
+}
+
+static void LogPeakMemory()
+{
+    using Process p = Process.GetCurrentProcess();
+    p.Refresh();
+
+    Console.WriteLine($"Working set: {FormatBytes(p.PeakWorkingSet64)} (peak)");
+    // Console.WriteLine($"Virtual:     {FormatBytes(p.PeakVirtualMemorySize64)} (peak)");
+}
 
 public static class Log
 {
@@ -341,6 +412,21 @@ public static class Log
         Info("  --test <path>                    test all files in the specified folder");
     }
 
+}
+
+enum TimerGroup
+{
+    TG_Compile      = 0,
+    TG_Preload      = 1,
+    TG_Lexer        = 2,
+    TG_Parser       = 3,
+    TG_Elab  = 4,
+    TG_Print        = 5,
+    TG_Analyser     = 6,
+    TG_Layout       = 7,
+    TG_ConstFold    = 8,
+    TG_IRGen        = 9,
+    TG_LLVM         = 10,
 }
 
 #endregion
